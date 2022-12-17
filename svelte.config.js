c1df723cfd9cf99bad020fb91c7ef6c3cea83416
Joseph Liu <josephliu24@outlook.com>
2022-12-17 01:59:54 -0500

    static

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 		adapter: adapter(),
 		appDir: 'app_',
 		paths: {
-			base: process.env.NODE_ENV === 'development' ? '' : '/sveltekit-static-repro'
+			base: process.env.NODE_ENV === 'development' ? '/' : '/sveltekit-static-repro'
 		}
 	},
 };
