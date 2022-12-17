29a88d5709b1d58e794497a4e79b86355c73d53d
Joseph Liu <josephliu24@outlook.com>
2022-12-17 01:35:03 -0500

    Initial Commit

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,3 @@
+<div>
+    this is a 404 page. it should look like the about page but with this content instead.
+</div>
---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+		appDir: 'app_',
+		paths: {
+			base: process.env.NODE_ENV === 'development' ? '' : '/sveltekit-static-repro'
+		}
+	},
+};
+
+export default config;
